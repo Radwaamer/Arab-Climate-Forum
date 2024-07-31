98af508ad503c85aec7d0a1b17df823730ac6a2f
Radwaamer <rororado44@gmail.com>
2024-08-01 02:24:39 +0300

    try to keep clean code

--- main.js
+++ main.js
@@ -124,7 +124,63 @@ document.querySelectorAll('input[name="btnradio"]').forEach(radio => {
 
 
 // nav bar mobile responsive
-(()=>{
+(()=>{``
+    
+    const header= document.querySelector("header");
+    const nav= document.querySelector(".nav");
+
+    const navContent= `
+    <nav class="navbar navbar-expand-lg bg-green rounded-top-4 w-100">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse mt-4 mt-lg-0 navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav justify-content-around flex-grow-1 text-center text-lg-end">
+                            <li class="nav-item">
+                                <a class="text-white nav-link active" aria-current="page" href="#">من نحن</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="text-white nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    الدورات السابقة
+                                </a>
+                                <ul class="dropdown-menu text-center text-lg-end">
+                                    <li><a class="text-white dropdown-item" href="#">الدورة الأولى</a></li>
+                                    <li><a class="text-white dropdown-item" href="#">الدورة الثانية</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="text-white nav-link" href="#">جرين بالعربي</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="text-white nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    المكتب الإعلامي
+                                </a>
+                                <ul class="dropdown-menu text-center text-lg-end">
+                                    <li><a class="text-white dropdown-item" href="#">الدورة الأولى</a></li>
+                                    <li><a class="text-white dropdown-item" href="#">الدورة الثانية</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="text-white nav-link" href="#">المكتبة الرقمية</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    `
+    
+    const logoDiv = document.createElement("div");
+    const signDiv = document.createElement("div");
+    const contactDiv = document.createElement("button");
+    logoDiv.innerHTML=`<img class="logo img-fluid" src="assets/logo.png" alt="">`;
+    signDiv.innerHTML=`<button class="btn btn-green border border-2 border-green py-0 px-4 text-green">تسجيل</button>`;
+    contactDiv.innerHTML=`
+                    <img src="assets/message.svg" alt="">
+                    <span class="text-green">تواصل معنا</span>`;
+    contactDiv.className=`btn d-flex align-items-center gap-2`;
+
+    // handle screen size
     let largeScreen=false;
     const getScreenSize= ()=>{
         if(window.innerWidth>992){
@@ -133,58 +189,9 @@ document.querySelectorAll('input[name="btnradio"]').forEach(radio => {
             largeScreen=false;
         }
     };
-const navContent= `
-<nav class="navbar navbar-expand-lg bg-green rounded-top-4 w-100">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse mt-4 mt-lg-0 navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav justify-content-around flex-grow-1 text-center text-lg-end">
-                        <li class="nav-item">
-                            <a class="text-white nav-link active" aria-current="page" href="#">من نحن</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="text-white nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                الدورات السابقة
-                            </a>
-                            <ul class="dropdown-menu text-center text-lg-end">
-                                <li><a class="text-white dropdown-item" href="#">الدورة الأولى</a></li>
-                                <li><a class="text-white dropdown-item" href="#">الدورة الثانية</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="text-white nav-link" href="#">جرين بالعربي</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="text-white nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                المكتب الإعلامي
-                            </a>
-                            <ul class="dropdown-menu text-center text-lg-end">
-                                <li><a class="text-white dropdown-item" href="#">الدورة الأولى</a></li>
-                                <li><a class="text-white dropdown-item" href="#">الدورة الثانية</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="text-white nav-link" href="#">المكتبة الرقمية</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-`
-const header= document.querySelector("header");
-const nav= document.querySelector(".nav");
 
-const logoDiv = document.createElement("div");
-const signDiv = document.createElement("div");
-const contactDiv = document.createElement("button");
-logoDiv.innerHTML=`<img class="logo img-fluid" src="assets/logo.png" alt="">`;
-signDiv.innerHTML=`<button class="btn btn-green border border-2 border-green py-0 px-4 text-green">تسجيل</button>`;
-contactDiv.innerHTML=`
-                <img src="assets/message.svg" alt="">
-                <span class="text-green">تواصل معنا</span>`;
-contactDiv.className=`btn d-flex align-items-center gap-2`;
+    // set nav bar
+
 const handleNav=()=>{
     if(largeScreen){
         nav.innerHTML=navContent;
@@ -203,6 +210,7 @@ const handleNav=()=>{
     }
 }
 
+// fire actions
 getScreenSize();
 handleNav();
 window.addEventListener('resize',()=>{
